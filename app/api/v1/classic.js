e43bcd151c4d351df68e815f557bb42a41905d94
郑智玮 <shzx1994529@gmail.com>
2019-05-25 22:43:59 +0800

    Add Model classic

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,10 +1,21 @@
 const Router = require('koa-router')
-const router = new Router()
-router.get('/v1/classic', (ctx, next) => {
-  console.log(ctx.request)
-  ctx.body = {
-    name: 'classic'
-  }
+const { Flow } = require('../../models/flow')
+const { Art } = require('../../models/art.js')
+const router = new Router({
+  prefix: '/v1/classic'
+})
+const { IntegerValidator } = require('../../validator/validator.js')
+const { Auth } = require('../../../middleware/auth')
+router.get('/latest', new Auth().m, async (ctx, next) => {
+  const flow = await Flow.findOne({
+    order: [
+      ['index', 'DESC']
+    ]
+  })
+  console.log(flow)
+  const art = await Art.getData(flow.art_id, flow.type)
+  art.setDataValue('index', flow.index)
+  ctx.body = art
 })
 
 module.exports = router
